1c3d749945d7a30922cc988081a51509ab86813f
lvlufei <lvlufei@aoji.cn>
2019-02-26 17:21:37 +0800

    Rxjava2.x实现轮询定时器

---
+++ utilslib/src/main/java/com/luffy/utilslib/utils/RxTimerUtils.java
@@ -0,0 +1,115 @@
+package com.luffy.utilslib.utils;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Created by lvlufei on 2019/2/26
+ *
+ * @desc Rxjava2.x实现轮询定时器
+ */
+public class RxTimerUtils {
+
+    private Disposable mDisposable;
+
+    private RxTimerUtils() {
+    }
+
+    public static RxTimerUtils getInstance() {
+        return RxTimerUtilsHelper.mRxTimerUtils;
+    }
+
+    private static class RxTimerUtilsHelper {
+        private static RxTimerUtils mRxTimerUtils;
+
+        static {
+            mRxTimerUtils = new RxTimerUtils();
+        }
+    }
+
+    /**
+     * 指定毫秒后执行next操作
+     *
+     * @param milliseconds
+     * @param next
+     */
+    public void timer(long milliseconds, final IRxNext next) {
+        Observable.timer(milliseconds, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable disposable) {
+                        mDisposable = disposable;
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Long number) {
+                        if (next != null) {
+                            next.doNext(number);
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        //取消订阅
+                        cancel();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        //取消订阅
+                        cancel();
+                    }
+                });
+    }
+
+    /**
+     * 每隔指定毫秒后执行next操作
+     *
+     * @param milliseconds
+     * @param next
+     */
+    public void interval(long milliseconds, final IRxNext next) {
+        Observable.interval(milliseconds, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable disposable) {
+                        mDisposable = disposable;
+                    }
+
+                    @Override
+                    public void onNext(@NonNull Long number) {
+                        if (next != null) {
+                            next.doNext(number);
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    public void cancel() {
+        if (mDisposable != null && !mDisposable.isDisposed()) {
+            mDisposable.dispose();
+        }
+    }
+
+    public interface IRxNext {
+        void doNext(long number);
+    }
+
+}
